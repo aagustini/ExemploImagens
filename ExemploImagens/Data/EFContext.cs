80c540cc075b7d260bb85e2ccfcff1a79ea8e8f8
AAgustini <alexandre.agustini@pucrs.br>
2020-10-31 08:09:38 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploImagens/Data/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExemploImagens.Models;
+
+namespace ExemploImagens.Data
+{
+    public class EFContext : DbContext
+    {
+        public EFContext (DbContextOptions<EFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        public DbSet<Imagem> Imagem { get; set; }
+    }
+}
---
+++ ExemploImagens/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploImagens.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public virtual ICollection<Imagem> Imagens { get; set; }
+    }
+}
